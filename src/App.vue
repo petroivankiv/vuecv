0058c9510dd13971f57eeb961b8e83e01829ffab
Petro Ivankiv <petro.ivankiv88@gmail.com>
2019-04-21 18:45:46 +0300

    Added header component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My new Vue.js App"/>
+    <Header name="Petro Ivankiv" position="Web Developer" />  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <img src="../assets/photo.png" alt="My Photo">
+    <div class="header-profile">
+      <span class="header-profile_name">{{ name }}</span>
+      <span>{{ position }}</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    name: String,
+    position: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+img { height: 100px; border-radius: 15%; }
+
+.header-profile {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 20px;
+}
+
+.header-profile_name {
+  font-size: 32px;
+  font-weight: bold;
+}
+</style>
