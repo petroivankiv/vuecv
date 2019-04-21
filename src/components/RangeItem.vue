18fe6fc26e8fd2f430b642ee57cba9dbb36aec99
Petro Ivankiv <petro.ivankiv88@gmail.com>
2019-04-21 23:07:21 +0300

    Added range component

---
+++ src/components/RangeItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <span>{{ title }}</span>
+    <vue-slider v-model="value" v-bind="getOptions" ref="slider"/>
+  </div>
+</template>
+
+<script>
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+
+export default {
+  name: "RangeItem",
+  components: {
+    VueSlider
+  },
+  props: {
+    value: String,
+    title: String
+  },
+  computed: {
+    getOptions: function() {
+      return {
+        dotSize: 4,
+        height: 4,
+        direction: "ltr",
+        min: 0,
+        max: 10,
+        interval: 1
+      };
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+span {
+  line-height: 1.5;
+  display: block;
+  text-align: start;
+}
+
+.vue-slider {
+  margin-top: 0;
+  margin-bottom: 7px;
+}
+</style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,41 +1,59 @@
 <template>
-  <div>
+  <div class="items-container">
     <span>Skills</span>
-    <span>Javascript</span>
-    <span>CSS3</span>
-    <span>HTML5</span>
-    <span>Typescript</span>
-    <span>MongoDB</span>
-    <span>NODEJS</span>
-    <span>Angular 6</span>
-    <span>React</span>
-    <span>Vue</span>
-    <span>Redux</span>
-    <span>NGRX</span>
-    <span>Git</span>
-    <span>Bitbucket</span>
-    <span>Git Lab</span>
-    <span>Git Lab CI</span>
-    <span>Travise</span>
+    <span v-for="item in items" :key="item.id">
+      <RangeItem :value="item.value" :title="item.title"/>
+    </span>
   </div>
 </template>
 
 <script>
+import RangeItem from "./RangeItem.vue";
+
 export default {
-  name: "Skills"
+  name: "Skills",
+  components: {
+    RangeItem
+  },
+  computed: {
+    items: function() {
+      return [
+        { id: "1", value: "9", title: "Javascript" },
+        { id: "2", value: "9", title: "CSS3" },
+        { id: "3", value: "9", title: "HTML5" },
+        { id: "4", value: "9", title: "Typescript" },
+        { id: "5", value: "5", title: "MongoDB" },
+        { id: "6", value: "6", title: "NODEJS" },
+        { id: "7", value: "8", title: "Angular6" },
+        { id: "8", value: "6", title: "React" },
+        { id: "9", value: "4", title: "Vue" },
+        { id: "10", value: "8", title: "Redux" },
+        { id: "11", value: "9", title: "NGRX" },
+        { id: "12", value: "9", title: "Git" },
+        { id: "13", value: "8", title: "Bitbucket" },
+        { id: "14", value: "8", title: "GitLab" },
+        { id: "15", value: "8", title: "GitLabCI" },
+        { id: "16", value: "6", title: "Travise" },
+      ];
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div {
-  display: flex;
-  flex-direction: column;
+.items-container {
   margin-top: 15px;
   font-size: 14px;
-  align-items: flex-start;
 }
 
-span { line-height: 1.5; }
-span:first-of-type { font-weight: bold; }
+span {
+  line-height: 1.5;
+  display: block;
+  text-align: start;
+}
+
+span:first-of-type {
+  font-weight: bold;
+}
 </style>
